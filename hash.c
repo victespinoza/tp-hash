1cd88d5ea4f1504e737e9d1df18093a4a4f66fe2
victor <victor.espinoza@etermax.com>
2020-06-24 18:26:03 -0300

    adds collisions case

--- hash.c
+++ hash.c
@@ -42,52 +42,45 @@ hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
     hash->tamanio = TAMANIO;
     hash->cantidad = 0;
     hash->tabla = malloc(sizeof(lista_t*)*TAMANIO);
-    if (!hash->tabla){
+    if (hash->tabla == NULL){
         free(hash);
         return NULL;
+    }
+    for (int i = 0; i < hash->tamanio; i++) {
+        hash->tabla[i] = lista_crear();
     }
     return hash;
 }
 
 size_t hash_cantidad(const hash_t *hash){
-	if (!hash){
-		return 0;
-	}
     return hash->cantidad;
 }
-size_t hash_tamanio(const hash_t *hash){
-	if (!hash){
-		return 0;
-	}
-    return hash->tamanio;
-}
+
 void hash_destruir(hash_t *hash){
-	if (!hash) {
-		for (size_t i = 0; i < hash_tamanio(hash); i++) {
-			if (hash->tabla[i]) {
-				lista_destruir(hash->tabla[i],hash->destruir_dato);
-			}
-		}
-		free(hash->tabla);
-	}
+    for (int i = 0; i < hash->tamanio; i++) {
+        lista_destruir(hash->tabla[i], hash->destruir_dato);
+    }
+    free(hash->tabla);
     free(hash);
 }
 
 bool hash_pertenece(const hash_t *hash, const char *clave) {
-    size_t posicion = hash_1(clave);
+    long posicion = hash_1(clave);
     clave_valor_t* actual;
     lista_iter_t* iter = lista_iter_crear(hash->tabla[posicion]);
     while (!lista_iter_al_final(iter)){
         actual = lista_iter_ver_actual(iter);
         if (strcmp(clave, actual->clave) == 0){
+            lista_iter_destruir(iter);
             return true;
         }
     }
+    lista_iter_destruir(iter);
     return false;
 }
 
 void *hash_obtener(const hash_t *hash, const char *clave){
-    size_t posicion = hash_1(clave);
+    long posicion = hash_1(clave);
     clave_valor_t* actual;
     lista_t* bucket = hash->tabla[posicion];
     lista_iter_t* iter = lista_iter_crear(bucket);
@@ -103,36 +96,51 @@ void *hash_obtener(const hash_t *hash, const char *clave){
 }
 
 bool hash_guardar(hash_t *hash, const char *clave, void *dato) {
-    size_t valor = hash_1(clave);
-    if (!hash->tabla[valor]) {
-        hash->tabla[valor] = lista_crear();
-    }
+
+    long valor = hash_1(clave);
     lista_t* bucket = hash->tabla[valor];
     clave_valor_t* actual;
     clave_valor_t* clave_valor = malloc(sizeof(clave_valor_t));
-    clave_valor->clave = malloc(sizeof(char)*(strlen(clave)+1));
+    if(clave_valor == NULL){
+        return false;
+    }
+    clave_valor->clave = malloc(sizeof(char)*strlen(clave));
+    if(clave_valor->clave == NULL){
+        free(clave_valor);
+        return false;
+    }
     strcpy(clave_valor->clave, clave );
     clave_valor->valor = dato;
-    lista_iter_t* iter = lista_iter_crear(bucket);
+    lista_iter_t* iter;
     if (lista_largo(bucket) < CAPACIDAD_BUCKETS){
-        for (;!lista_iter_al_final(iter); lista_iter_avanzar(iter)) {
+        iter = lista_iter_crear(bucket);
+        for (int i = 0; i < CAPACIDAD_BUCKETS && !lista_iter_al_final(iter); i++) {
             actual = lista_iter_ver_actual(iter);
             if (strcmp(actual->clave, clave) == 0){
+                free(actual->clave);
                 lista_iter_borrar(iter);
                 hash->cantidad--;
                 break;
             }
+            lista_iter_avanzar(iter);
         }
         lista_iter_insertar(iter, clave_valor);
+        lista_iter_destruir(iter);
+        hash->cantidad++;
+        return true;
     }
-    lista_iter_destruir(iter);
-    hash->cantidad++;
-    return true;
+    else{
+        iter = lista_iter_crear(bucket);
+        clave_valor_t* primer_elemento = lista_iter_borrar(iter);
+        lista_iter_insertar(iter, clave_valor);
+        lista_iter_destruir(iter);
+        return hash_guardar(hash,primer_elemento->clave, primer_elemento->valor);
+    }
 }
 
 void *hash_borrar(hash_t *hash, const char *clave){
-    size_t valor = hash_1(clave);
-    lista_t* bucket = hash->tabla[valor];
+    long posicion = hash_1(clave);
+    lista_t* bucket = hash->tabla[posicion];
     if (bucket == NULL) {
         return NULL;
     }
@@ -141,6 +149,7 @@ void *hash_borrar(hash_t *hash, const char *clave){
     while (!lista_iter_al_final(iter)){
         actual = lista_iter_ver_actual(iter);
         if(strcmp(clave, actual->clave) == 0){
+            free(actual->clave);
             lista_iter_borrar(iter);
             return actual->valor;
         }
@@ -326,7 +335,6 @@ void hash_iter_destruir(hash_iter_t* iter) {
 }
 
 extern void destruir_clave_valor(void *tda) {
-	printf("imprime esto\n");
 	clave_valor_t* estructura = tda;
 	free(estructura->clave);
 	free(estructura->valor);
